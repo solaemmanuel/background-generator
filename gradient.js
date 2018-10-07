789dc2231774d63dfd128320fd2a71f161b7a184
solaemmanuel <sholimagaduski@gmail.com>
2018-10-07 03:34:36 +0100

    adding a new file

---
+++ gradient.js
@@ -0,0 +1,18 @@
+var h2 = document.querySelector("h2");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.querySelector("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, "
+	+ color1.value 
+	+", "
+	+ color2.value 
+	+")"; 
+css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
